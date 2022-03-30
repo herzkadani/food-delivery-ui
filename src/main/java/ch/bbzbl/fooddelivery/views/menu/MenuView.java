50ba1a3aaaa24ef6caf293252b777b9d8f303c80
start.vaadin.com <no-reply@vaadin.com>
2022-03-30 11:34:07 +0000

    Generated project

---
+++ src/main/java/ch/bbzbl/fooddelivery/views/menu/MenuView.java
@@ -0,0 +1,32 @@
+package ch.bbzbl.fooddelivery.views.menu;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Menu")
+@Route(value = "menu")
+@RouteAlias(value = "")
+public class MenuView extends VerticalLayout {
+
+    public MenuView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
